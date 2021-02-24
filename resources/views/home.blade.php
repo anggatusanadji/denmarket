6f7d0337c66ad11ef834409a990146b4047bdae5
anggatusanadji <anggatusanadji@gmal.com>
2021-02-24 16:44:49 +0800

    design homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,87 @@
 @section('title', 'Home')
 
 @section('container')
-    <div class="hero">
-        <div class="row reverse-column mt-3 h-100">
-            <div class="hero-text col-lg-5 col-sm-12 text-center">
-                <p class="font-weight-normal" style="font-size: 30px">Welcome To DenMarket Sport   </p>
-                <p class="font-weight-bold" style="font-size: 40px; line-height:40px">It’s okay to be a little obsessed with shoes!</p>
-                <p class="font-weight-bold" style="font-size: 24pxpx">be a force in the court with your shoes</p>
+    <div class="hero mt-2">
+        <div class="row reverse-column h-100">
+            <div class="hero-text col-lg-5 col-md-5 col-12 mt-5 d-flex flex-column justify-content-center">
+                <div>
+                    <p class="font-weight-normal welcome-hero">Welcome To DenMarket Sport</p>
+                    <p class="font-weight-bold title-hero">It’s okay to be a little obsessed with shoes!</p>
+                    <p class="font-weight-normal text-hero">be a force in the court with your shoes</p>
+                </div>
+                <div class="explore">
+                    <a type="button" class="btn mr-2" ><i class="fas fa-chevron-down" style="color:#fff;"></i></a>
+                    <a class="font-weight-normal mr-2">Explore More</a>
+                    <img src="{{ asset('image/line.svg') }}" alt="" style="width:30px;">
+                </div>
+                <div class="elipse mt-5" >
+                    <img src="{{ asset('image/Elipse1.png') }}" alt="" style="height:80px">
+                </div>
             </div>
-            <div class="col-lg-7 col-sm-12 img">
-                <img src="{{ asset('image/Hero.png') }}" alt="">
+            <div class="col-lg-7 col-12 img">
+                <img src="{{ asset('image/Hero.png') }}"  alt="">
             </div>
+        </div>
+    </div>
+
+    <div class="quality">
+        <div class="row">
+            <div class="col-lg-6 col-12 img">
+                <img src="{{ asset('image/MaskGroup.png') }}" alt="" class="w-100">
+            </div>
+            <div class="col-lg-6 col-12 d-flex flex-column justify-content-center">
+                <p class="font-weight-bold title-quality">With quality ingredients</p>
+                <p class="font-weight-normal text-quality">a great fit for powerful players who want good ankle support from a shoe that still feels light. The sculpted, padded collar and exterior heel counter provide a stable fit, while visible cushioning units under the forefoot return energy with every step. </p>
+            </div>  
+        </div>
+    </div>
+
+    <div class="trending">
+        <div class="row">
+            <p class="font-weight-bold trending-title ml-auto"><span style="color: #FF4A4A">Trending</span> Collection</p>
+        </div>
+        <div class="row justify-content-center">
+            <div class="card-deck col-lg-12">
+                <div class="card col-lg-6 col-12">
+                    <img src="{{ asset('image/futsal1.jpg') }}" alt="" class="w-100 h-100">
+                </div>
+                <div class="card col-lg-6 col-12">
+                    <img src="{{ asset('image/futsal2.jpg') }}" alt="" class="w-100 h-100">
+                </div>
+
+                <div class="card col-lg-6 col-12">
+                    <img src="{{ asset('image/futsal3.jpg') }}" alt="" class="w-100 h-100">
+                </div>
+
+                <div class="card col-lg-6 col-12">
+                    <img src="{{ asset('image/futsal4.jpg') }}" alt="" class="w-100 h-100">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="discount">
+        <div class="card">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6 p-5">
+                    <img src="{{ asset('image/discount banner.png') }}" alt="" class="w-100">
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 d-flex flex-column justify-content-center">
+                    <p class="font-weight-bold title-discount">Get Your Shoes Now On <span style="color:#FF4A4A">15%</span> Discount</p>
+                    <p class="font-weight-normal text-discount">Come on, choose your shoes immediately</p>
+                    <div class="button-buy mt-2">
+                        <a type="button" class="btn mr-2" ><i class="fas fa-plus" style="color:#fff;"></i></a>
+                        <a class="font-weight-normal mr-2">Buy Now</a>
+                        <img src="{{ asset('image/line.svg') }}" alt="" style="width:30px;">
+                    </div>
+                </div>
+                <div class="col-lg-12 show">
+                    <a type="button" class="btn mr-2" ><i class="fas fa-chevron-down" style="color:#fff;"></i></a>
+                    <a class="font-weight-normal mr-2">Show More</a>
+                    <img src="{{ asset('image/line.svg') }}" alt="" style="width:30px;">
+                </div>
+            </div>
+           
         </div>
     </div>
 @endsection
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -8,33 +8,34 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> 
         <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
-
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+        
         <title>@yield('title')</title>
     </head>
     <body>
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light">
-            <a class="navbar-brand" href="#"><span style=color:#628BF6>DenMarket</span> Store</a>
+            <a class="navbar-brand font-weight-bold" href="#"><span style=color:#628BF6>DenMarket</span> Store</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Product</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Location</a>
-                </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Product</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Location</a>
+                    </li>
                 </ul>
             </div>
         </nav>
@@ -49,5 +50,6 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
     </body>
 </html>
