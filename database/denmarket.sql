6861e339035d16e74e7ca4b90899c3f2142d494c
anggatusanadji <anggatusanadji@gmal.com>
2021-03-06 15:08:10 +0800

    update database

--- database/denmarket.sql
+++ database/denmarket.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 06 Mar 2021 pada 02.45
+-- Waktu pembuatan: 06 Mar 2021 pada 08.07
 -- Versi server: 10.4.17-MariaDB
 -- Versi PHP: 8.0.2
 
@@ -128,11 +128,11 @@ CREATE TABLE `tb_produk` (
 
 INSERT INTO `tb_produk` (`id`, `nama_produk`, `deskripsi_produk`, `kategori_id`, `size_produk`, `harga_produk`, `tipe_id`, `gambar_produk`, `created_at`, `updated_at`, `deleted_at`) VALUES
 (1, 'Adidas c7', 'Shoes that use white dominance with blue patterns and also the Adidas logo on the right and left of the shoes are black', 2, '43, 44, 45, 46', '300000', 3, '1614829642.png', '2021-03-03 19:47:22', '2021-03-04 17:40:20', NULL),
-(3, 'New Balance Furon 4.0', 'Shoes that use black dominance with a green gradient towards the front, these shoes use the number 1 quality of material, very light and comfortable to wear.', 2, '43 ,44, 45', '279000', 3, '1614904795.png', '2021-03-03 19:55:49', '2021-03-05 17:06:44', NULL),
+(3, 'New Balance Furon 4.0', 'Shoes that use black dominance with a green gradient towards the front, these shoes use the number 1 quality of material, very light and comfortable to wear.', 2, '43 ,44, 45', '279000', 1, '1614904795.png', '2021-03-03 19:55:49', '2021-03-05 19:09:56', NULL),
 (4, 'Nike Mercurial Superfly 7 Elite FG', '360-degree Flyknit construction wraps your foot for a second-skin fit. New high-tenacity yarn and a 1-piece lining create a close, secure feel on the field. A new ultra-thin NIKESKIN overlay gets you closer to the ball for high-speed maneuvers', 2, '43 , 44, 45, 46', '599000', 1, '1614904970.png', '2021-03-04 16:42:50', '2021-03-04 16:42:50', NULL),
 (5, 'Adidas Dribble Fly J4', 'Shoes that use black dominance by containing a purple part under the back of the shoe and the top of the shoe, a shoe that looks very elegant for you to play on the field', 2, '43, 44, 45, 46, 47', '450000', 1, '1614905210.png', '2021-03-04 16:46:50', '2021-03-04 17:42:23', NULL),
 (6, 'Adidas White Simple F5', 'Shoes that create an elegant impression with a fairly simple design, dominated by white and a pattern on the side of the shoe, and contain a logo on the front', 2, '42, 43, 44, 45, 46, 47', '330000', 1, '1614932519.png', '2021-03-04 16:56:21', '2021-03-05 00:21:59', NULL),
-(7, 'Nike Vapor XII Elite White', 'Shoes that use white dominance with a black pattern on the front, prioritizing the quality of contemporary designs, make you look more powerful in the field', 2, '41, 42, 43, 44, 45', '450000', 3, '1614944010.png', '2021-03-05 03:33:30', '2021-03-05 03:33:30', NULL),
+(7, 'Nike Vapor XII Elite White', 'Shoes that use white dominance with a black pattern on the front, prioritizing the quality of contemporary designs, make you look more powerful in the field', 2, '41, 42, 43, 44, 45', '450000', 1, '1614944010.png', '2021-03-05 03:33:30', '2021-03-05 19:10:05', NULL),
 (8, 'Adidas Predator', 'You can\'t change the game till you let the game change you. Every match is a chance to be bigger, better. More in control. Unleash your full force of nature in Predator Freak.', 2, '43, 44, 45, 46', '899000', 3, '1614944282.png', '2021-03-05 03:38:02', '2021-03-05 03:38:02', NULL),
 (9, 'New Balance Visaro 2.0', 'The playmakers’ choice; for the artists of the game, who strive to perform with daring creativity. The Visaro 2.0 features a performance ergonomic last with a lower toe spring and a PU synthetic upper for superior fit and comfort.', 2, '43, 44, 45, 46', '500000', 1, '1614944662.png', '2021-03-05 03:44:22', '2021-03-05 16:18:07', NULL),
 (10, 'Nike Elastico Collection', 'Take our most innovative boot to the next level in the bike elastico. After analyzing thousands of footballs specific moves, a material was added to the flyknit to give you better touch and control of the ball', 1, '43, 44, 45, 46', '799000', 3, '1614945536.png', '2021-03-05 03:58:56', '2021-03-05 03:58:56', NULL),
@@ -194,7 +194,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `username`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Angga Tusan Adji', 'anggatus', 'anggatusanadji@gmail.com', NULL, '$2y$10$7sGkviWyBcWYHr5tOoZb9.6egWBYidR1zJs8jI6R1mg2ZeGlr3K0q', 'ThQDm8fUkxiYFSJYMa1IwDXPH0ZWBjAlABRlENcsTL9hVKhlp8TmYG670igT', '2021-02-28 06:11:30', '2021-02-28 06:11:30');
+(1, 'Angga Tusan Adji', 'anggatus', 'anggatusanadji@gmail.com', NULL, '$2y$10$7sGkviWyBcWYHr5tOoZb9.6egWBYidR1zJs8jI6R1mg2ZeGlr3K0q', 'NX6XArxzvA1xLFx32my220HVIX7jbk8r10jv8hw6VFNPxlcKhFaxyCyRcYSK', '2021-02-28 06:11:30', '2021-02-28 06:11:30'),
+(2, 'Admin', 'admin', 'admin123@gmail.com', NULL, '$2y$10$XmQfqcI5jLWehStR6Nz2/.LutyHWkuFGAxZ/rr/CFTFf32gn1WjU.', NULL, '2021-03-05 23:04:38', '2021-03-05 23:04:38');
 
 --
 -- Indexes for dumped tables
@@ -289,7 +290,7 @@ ALTER TABLE `tb_tipe_produk`
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
